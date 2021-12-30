fa49650bbecc09a08ad9cab68cf1c5e0615568a8
Denise-E <denueichen@gmail.com>
2021-12-30 17:33:08 -0300

    creando páginas productos según categorias

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -15,6 +15,32 @@ module.exports = {
     categories: (req,res) => res.render("products/categories",{
         products: products,
         style: "categories"
-    })
+    }),
+
+    gastronomia: (req,res) => res.render("products/categories/gastronomia",{
+        products: products,
+        style: "gastronomia"
+    }),
+
+    aventura: (req,res) => res.render("products/categories/aventura",{
+        products: products,
+        style: "aventura"
+    }),
+
+    estarBien: (req,res) => res.render("products/categories/estarBien",{
+        products: products,
+        style: "estarBien"
+    }),
+
+    entretenimiento: (req,res) => res.render("products/categories/entretenimiento",{
+        products: products,
+        style: "entretenimiento"
+    }),
+
+    estadias: (req,res) => res.render("products/categories/estadias",{
+        products: products,
+        style: "estadias"
+    }),
+
 
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,5 +5,10 @@ const router = express.Router();
 router.get("/productDetail", products.productDetail);
 router.get("/cart", products.cart);
 router.get("/products/categories", products.categories);
+router.get("/products/categories/gastronomia", products.gastronomia);
+router.get("/products/categories/aventura", products.aventura);
+router.get("/products/categories/estarBien", products.estarBien);
+router.get("/products/categories/entretenimeinto", products.entretenimiento);
+router.get("/products/categories/estadias", products.estadias);
 
 module.exports = router;
