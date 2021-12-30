84e69ff52101260f12512b0e399db1123df3e456
Denise-E <denueichen@gmail.com>
2021-12-30 17:02:18 -0300

    agregando página de categorías

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,16 +1,20 @@
 
 const products = require("../data/products");
-const productos = require("../data/products");
 
 module.exports = {
     productDetail: (req,res) => res.render("products/productDetail",{
-        products: productos,
+        products: products,
         style: "productDetail"
     }),
 
     cart: (req,res) => res.render("products/cart",{
      
         style: "cart"
+    }),
+    
+    categories: (req,res) => res.render("products/categories",{
+        products: products,
+        style: "categories"
     })
 
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/productDetail", products.productDetail);
 router.get("/cart", products.cart);
+router.get("/products/categories", products.categories);
 
 module.exports = router;
