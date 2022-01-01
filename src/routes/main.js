f1b9e91c3e90b7ab473df6309433f41938d650ce
melisci <melisascime@gmail.com>
2022-01-01 19:58:56 -0300

    cambios

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,6 +3,6 @@ const main = require("../controllers/main");
 const router = express.Router();
 
 router.get("/", main.index);
-router.get("/comofunciona", main.comofunciona);
+
 
 module.exports = router;
