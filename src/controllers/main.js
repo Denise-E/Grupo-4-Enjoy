f975a6b331a9865a49b6b842c6de480b04160c33
Denise-E <denueichen@gmail.com>
2022-01-02 11:36:07 -0300

    creando rutas y docs a links del footer

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -5,5 +5,45 @@ module.exports = {
     index: (req,res) => res.render("index",{
      
         style: "index"
+    }),
+    quienesSomos: (req,res) => res.render("footerLinks/quienesSomos",{
+     
+        style: "footerLinks/quienesSomos"
+    }),
+    trabajarJuntos: (req,res) => res.render("footerLinks/trabajarJuntos",{
+     
+        style: "footerLinks/trabajarJuntos"
+    }),
+    puntosVenta: (req,res) => res.render("footerLinks/puntosVenta",{
+     
+        style: "footerLinks/puntosVenta"
+    }),
+    tuExperiencia: (req,res) => res.render("footerLinks/tuExperiencia",{
+     
+        style: "footerLinks/tuExperiencia"
+    }),
+    comoComprar: (req,res) => res.render("footerLinks/comoComprar",{
+     
+        style: "footerLinks/comoComprar"
+    }),
+    contacto: (req,res) => res.render("footerLinks/contacto",{
+     
+        style: "footerLinks/contacto"
+    }),
+    preguntas: (req,res) => res.render("footerLinks/preguntas",{
+     
+        style: "footerLinks/preguntas"
+    }),
+    devoluciones: (req,res) => res.render("footerLinks/devoluciones",{
+     
+        style: "footerLinks/devoluciones"
+    }),
+    politicasGarantias: (req,res) => res.render("footerLinks/politicasGarantias",{
+     
+        style: "footerLinks/politicasGarantias"
+    }),
+    terminosCondiciones: (req,res) => res.render("footerLinks/terminosCondiciones",{
+     
+        style: "footerLinks/terminosCondiciones"
     })   
 }
--- src/routes/main.js
+++ src/routes/main.js
@@ -3,6 +3,16 @@ const main = require("../controllers/main");
 const router = express.Router();
 
 router.get("/", main.index);
+router.get("/quienesSomos", main.quienesSomos);
+router.get("/trabajeConNosotros", main.trabajarJuntos);
+router.get("/puntosVenta", main.puntosVenta);
+router.get("/uniTuExperiencia", main.tuExperiencia);
+router.get("/comoComprar", main.comoComprar);
+router.get("/contacto", main.contacto);
+router.get("/preguntasFrecuentes", main.preguntas);
+router.get("/devoluciones", main.devoluciones);
+router.get("/politicas&garantias", main.politicasGarantias);
+router.get("/terminos&condiciones", main.terminosCondiciones);
 
 
 module.exports = router;
