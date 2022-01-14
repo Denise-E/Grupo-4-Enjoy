da0574dd50517ca0f689bfcd9e6b54255990d59c
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-01-14 11:46:30 -0300

    subiendo ejs de productos por categoría

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,12 +1,14 @@
 const products = require("../data/products");
 const categories = require("../data/categories");
-//const models = require("../models/products"); Hbaría que crearlo
+//const models = require("../models/products"); Habaría que crearlo
 
 module.exports ={
     index:(req,res) => res.render("categories/index",{
         style: "categories/index"
     }),
     categories:(req,res) => res.render("categories/categories",{
+        products: products,
+        category: categories.find(category => category.id == req.params.id),
         style: "categories/categories"
     }),
 }
