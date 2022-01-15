a3831e0238226d82e759645796090ffb1666fed7
julierrante <julietaerrante21@gmail.com>
2022-01-14 21:28:42 -0300

    subiendo experiencias corporativas

--- src/app.js
+++ src/app.js
@@ -19,3 +19,4 @@ app.use("/products", require("./routes/products"));
 app.use(require("./routes/users"));
 app.use("/admins", require("./routes/admin"));
 app.use("/categories", require("./routes/categories"));
+
