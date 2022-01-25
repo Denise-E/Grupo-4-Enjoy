e3d5087733d3e9b57602bf2fda6752054372c816
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-01-25 16:02:21 -0300

    instalando paquetes para usuarios

--- src/app.js
+++ src/app.js
@@ -2,21 +2,30 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const method = require("method-override");
+const cookie = require("cookie-parser");
+const session = require("express-session");
 
 app.set("port", process.env.PORT || 3000);
 app.set ("views", path.resolve (__dirname, "views"));
 app.set ("view engine", "ejs");
+
 app.listen(app.get("port"), ()=> console.log("Listening on port http://localhost:" + app.get("port")));
 
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.use("/uploads",express.static(path.resolve(__dirname, "../uploads")));
 app.use(method("m"));
 app.use(express.urlencoded({extended:true}));
+app.use(cookie());
+app.use(session({
+    secret:"Secret",
+    saveUninitialized: true,
+    resave: false,
+}))
 
 
 app.use(require("./routes/main"));
 app.use("/products", require("./routes/products"));
-app.use(require("./routes/users"));
+app.use("/users",require("./routes/users"));
 app.use("/admins", require("./routes/admin"));
 app.use("/categories", require("./routes/categories"));
 app.use ("/files", require("./routes/files"));
