7b611a9f21b55e5760c083c2f88311041896b98e
melisci <melisascime@gmail.com>
2022-02-15 14:42:41 -0300

    acces

---
+++ src/middlewares/access.js
@@ -0,0 +1,3 @@
+module.exports= (req,res,next)  => req.session && req.session.user ? next() : res.redirect ("/users/login")
+  
+
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,15 +8,16 @@ const upload = multer ({storage: multer.diskStorage({
     destination: (req, file, cb) => cb(null, path.resolve (__dirname, "../../uploads")),
       filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)),
 })});
+const access = require ("../middlewares/access");
 
 
 
 
 router.get("/login", users.login);
 router.get("/register", users.register);
-router.get("/list", users.list); //Listado de TODOS los ususarios
-router.get("/editarUsuario/:id", users.editarUsuario);
-router.get("/:id", users.show); //ruta dinamica
+router.get("/list",[access], users.list); //Listado de TODOS los ususarios
+router.get("/editarUsuario/:id",[access], users.editarUsuario);
+router.get("/:id",[access], users.show); //ruta dinamica
 
 
 router.post("/",[validate, upload.single()], users.save);
