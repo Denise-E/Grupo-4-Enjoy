1998cde2776394c18fb18e78b0396dfc6bd5408c
María laura Alvarez <marialau520@gmail.com>
2022-03-04 11:55:50 -0300

    actualizando sequelize

--- database/models/Modelo.js
+++ database/models/Modelo.js
@@ -1,4 +1,4 @@
-module.exports= (Sequelize,DataTypes) => {
+module.exports= (Sequelize,Datatypes) => {
 const alias = "Modelo" //sirve para consultar o pedir datos sobre una tabla.
 const cols = {
 id:{
@@ -6,13 +6,48 @@ id:{
     autoIncrement:true, 
     type: Datatypes.INTEGER,
 },
-id:{
-    primaryKey:true,
-    autoIncrement:true, 
+name:{
+    notNull:false,
+    type: Datatypes.STRING,
+},
+title:{
+    notNull:false,
+    type: Datatypes.STRING,
+},
+description:{
+    notNull:false,
+    type: Datatypes.STRING,
+},
+resume:{
+    notNull:false,
+    type: Datatypes.STRING,
+},
+location:{
+    notNull:false,
+    type: Datatypes.STRING,
+},
+persons:{
+    notNull:false,
     type: Datatypes.INTEGER,
 },
-
+expirationDate:{
+    notNull:false,
+    type: Datatypes.DATE,
+},
+price:{
+    notNull:false,
+    type: Datatypes.INTEGER,
+},
+fees:{
+    notNull:false,
+    type: Datatypes.INTEGER,
+},
+starsAvg:{
+    notNull:false,
+    type: Datatypes.INTEGER,
+},
     }
+
  const config = {
      timestamps: false, //son las columnas de created_at y updated_at. (si lo utiizamos va true, sino queda false)
 
