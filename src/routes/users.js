482be796e8f75435a36a064b0f55020dac24d859
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-03 17:04:34 -0300

    agregando middlewares rutas usuarios

--- src/routes/users.js
+++ src/routes/users.js
@@ -17,9 +17,9 @@ const validates = require("../validations/users.js")
 
 router.get("/login", users.login);
 router.get("/register", users.register);
-router.get("/list", users.list); //,[auth]
-router.get("/editarUsuario/:id", users.editarUsuario);//,[access]
-router.get("/:id", users.show); //,[access]
+router.get("/list",[auth], users.list); 
+router.get("/editarUsuario/:id",[access], users.editarUsuario);
+router.get("/:id",[access], users.show); 
 
 
 router.post("/",[upload.single("image")], users.save); //, validates.register
