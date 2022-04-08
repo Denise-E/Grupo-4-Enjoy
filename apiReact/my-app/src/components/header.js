193b7ab8f1d9b4fd8dc0ff37c2113b816e329646
melisci <melisascime@gmail.com>
2022-04-08 14:36:57 -0300

    apiReact

--- apiReact/my-app/src/components/header.js
+++ apiReact/my-app/src/components/header.js
@@ -4,14 +4,15 @@ import logo from '../assets/img/ENJOY.png';
 function Encabezado(){
     return (
         <header className="encabezado">
-            <nav id="logo">
-                <a href="#imagen"><img src={logo} alt="Logo ENJOY"/></a>
-            </nav>
-            <nav id="opciones">
+            
+            <nav id="opciones" class="flexRow">
                 <ul>
-                    <li><a href="#Inicio">Inicio</a></li>
-                    <li><a href="#Nosotras">Categorías</a></li>
+                    
+                    
                     <li><a href="#productos">Productos</a></li>
+                    <nav id="logo">
+                <a href="#imagen"><img src={logo} alt="Logo ENJOY"/></a>
+                    </nav>
                     <li><a href="#usuarios">Usarios</a></li>
                 </ul>
             </nav>
