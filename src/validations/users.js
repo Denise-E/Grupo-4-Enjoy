d58a6737677d8246dff915a6efb8c36a603bcb87
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-10 21:35:53 -0300

    subiendo ajustes en users register

--- src/validations/users.js
+++ src/validations/users.js
@@ -9,9 +9,7 @@ module.exports ={
     register: [
         validator.body("firstName").notEmpty().withMessage("Debe tener como mínimo 2 caracteres").isLength({min:2}),
         validator.body("lastName").notEmpty().withMessage("Debe tener como mínimo 2 caracteres").isLength({min:2}),
-    
-        validator.body("email").notEmpty().withMessage("Debe tener como mínimo 2 caracteres").isEmail(),
-        
+        validator.body("email").notEmpty().withMessage("Escriba un email válido").isEmail(),
         validator.body("password").notEmpty().withMessage("Debe tener como mínimo 8 caracteres").isLength({min:8}),
         validator.body('image').custom((value,{req})=>{
             if (req.file) {
