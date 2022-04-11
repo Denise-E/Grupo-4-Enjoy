158936a37ab2c4b0a0cf8870216ab769927329ad
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-04-11 16:07:52 -0300

    arreglando validaciones back register

--- src/validations/users.js
+++ src/validations/users.js
@@ -7,10 +7,10 @@ module.exports ={
         validator.body("email").notEmpty().withMessage("Este campo es obligatorio"),
     ],
     register: [
-        validator.body("firstName").notEmpty().withMessage("Debe tener como mínimo 2 caracteres").isLength({min:2}),
-        validator.body("lastName").notEmpty().withMessage("Debe tener como mínimo 2 caracteres").isLength({min:2}),
-        validator.body("email").notEmpty().withMessage("Escriba un email válido").isEmail(),
-        validator.body("password").notEmpty().withMessage("Debe tener como mínimo 8 caracteres").isLength({min:8}),
+        validator.body("firstName").notEmpty().isLength({min:2}).withMessage("Debe tener como mínimo 2 caracteres"),
+        validator.body("lastName").notEmpty().isLength({min:2}).withMessage("Debe tener como mínimo 2 caracteres"),
+        validator.body("email").notEmpty().isEmail().withMessage("Escriba un email válido"),
+        validator.body("password").notEmpty().isLength({min:8}).withMessage("Debe tener como mínimo 8 caracteres"),
         validator.body('image').custom((value,{req})=>{
             if (req.file) {
                 let allowedExtensions = ['.jpeg','.jpg','.gif','.png'];
