fd766ac480dd35f91739d325d2976162f7da8ebc
melisci <melisascime@gmail.com>
2022-04-12 09:12:26 -0300

    apireact

--- apiReact/my-app/src/components/header.js
+++ apiReact/my-app/src/components/header.js
@@ -12,14 +12,14 @@ function Encabezado(){
                 <ul className="flexRow">
                     
                     
-                    <li><Link to ="/products">Productos</Link></li>
+                    {/* <li><Link to ="/products">Productos</Link></li>
                     <Routes>
                         <Route path="/products" element={<Productstable />} />
-                    </Routes>
+                    </Routes> */}
                     <nav id="logo">
                 <a href="#imagen"><img src={logo} alt="Logo ENJOY"/></a>
                     </nav>
-                    <li><Link to ="/">Usuarios</Link></li>
+                    {/* <li><Link to ="/">Usuarios</Link></li> */}
                     
                 </ul>
             </nav>
